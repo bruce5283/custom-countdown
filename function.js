a6400a67d55877ee01b80211b2185fd4de121a8e
bruce5283 <bcarrington7@gmail.com>
2022-02-17 15:55:14 -0500

    Update function.js

--- function.js
+++ function.js
@@ -65,7 +65,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = minutes + "m " + seconds + "seconds";
+  document.getElementById("demo").innerHTML = minutes + "m " + seconds + "l";
 
   // If the count down is finished, write some text
   if (distance < 0) {
