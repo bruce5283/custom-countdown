3011abe9712269ed136e24cd85f979147605b4b8
bruce5283 <bcarrington7@gmail.com>
2022-04-24 10:47:41 -0400

    Updated

--- function.js
+++ function.js
@@ -51,6 +51,8 @@ color: #12A89E;
 // Set the date we're counting down to
 var countDownDate = new Date("${time}").getTime();
 
+var timeoutAudio = document.getElementByID("timeout_audio");
+
 
 // Update the count down every 1 second
 var x = setInterval(function() {
